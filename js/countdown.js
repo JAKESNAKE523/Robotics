cb564dd355d7b89781a53c3dc540cf8baebdf18b
Jakesnake523 <unconfigured@null.spigotmc.org>
2019-03-22 23:00:12 -0500

    Reorganizing Repo

--- js/countdown.js
+++ js/countdown.js
@@ -1,1 +1,30 @@
-var interval=setInterval(displayRemaining,1);function displayRemaining(){event=new Date(2018,10,3,8),remaining=event-new Date,days=Math.floor(remaining/864e5),hours=Math.floor(remaining/36e5-24*days),minutes=Math.floor(remaining/6e4-(60*hours+24*days*60)),seconds=Math.floor(remaining/1e3-(60*minutes+60*hours*60+60*days*60*24)),milliseconds=Math.floor(remaining-(60*minutes*1e3+60*hours*60*1e3+60*days*60*24*1e3+1e3*seconds)),final=days+":"+pad(hours,0)+":"+pad(minutes,0)+":"+pad(seconds,0)+":"+pad(milliseconds,1),document.getElementById("countdown").innerHTML=final,remaining<=0&&(remaining<-864e5?document.getElementById("countdown").innerHTML="Event over!":document.getElementById("countdown").innerHTML="Event started!",clearInterval(interval))}function pad(n,e){return n<10&&1==e?"00"+n:n<100&&1==e?"0"+n:n<10&&0==e?"0"+n:0<n?n:"000"}
+var interval = setInterval(displayRemaining, 1);
+function displayRemaining() {
+  event = new Date(2018, 10, 3, 8);
+  remaining = event - new Date();
+  days = Math.floor(remaining / (24*60*60*1000));
+  hours = Math.floor((remaining / (60*60*1000)) - (days*(24)));
+  minutes = Math.floor((remaining / (60*1000)) - ((hours * 60)+(days*24*60)));
+  seconds = Math.floor((remaining / 1000) - ((minutes * 60) + (hours*60*60)+(days*60*60*24)));
+  milliseconds = (Math.floor((remaining) - ((minutes * 60*1000) + (hours*60*60*1000)+(days*60*60*24*1000)+(seconds*1000))));
+  final = days + ':' + pad(hours, 0) + ':' + pad(minutes, 0) + ':' + pad(seconds, 0) +':' + pad(milliseconds, 1);
+
+  document.getElementById('countdown').innerHTML = final;
+  if (remaining <= 0) {
+    remaining < -(24*60*60*1000) ? document.getElementById('countdown').innerHTML = "Event over!" :document.getElementById('countdown').innerHTML = 'Event started!';
+    clearInterval(interval);
+  }
+}
+function pad(n, p) {
+    if (n<10 && p==1) {
+      return "00" + n;
+    }else if (n < 100 && p==1) {
+      return "0" + n;
+    }else if (n < 10 && p==0){
+      return "0" + n;
+    }else if (n > 0) {
+      return n;
+    }else {
+      return "000";
+    }
+}
